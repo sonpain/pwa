40ea7f50cf188dbf2dbf2c3194725afac1d78181
sonpain <mageson@naver.com>
2021-05-11 19:16:27 +0900

    dsf

--- js/common.js
+++ js/common.js
@@ -2,20 +2,20 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/pwa/service-worker.js').then((registration) => {
     // 업데이트 발견
-  //   registration.addEventListener('updatefound', () => {
-  //     // 설치 중인 새로운 서비스 워커
-  //     const newServiceWorker = registration.installing;
-  //     console.log('PAPER: New update found!');
+    registration.addEventListener('updatefound', () => {
+      // 설치 중인 새로운 서비스 워커
+      const newServiceWorker = registration.installing;
+      console.log('PAPER: New update found!');
 
-  //     newServiceWorker.addEventListener('statechange', (event) => {
-  //       const state = event.target.state;
-  //       console.log('PAPER: ' + state);
-  //       if (state === 'installed') {
-  //         util.message('앱을 재시작하면 업데이트가 적용됩니다!');
-  //       }
-  //     });
-  //   });
- });
+      newServiceWorker.addEventListener('statechange', (event) => {
+        const state = event.target.state;
+        console.log('PAPER: ' + state);
+        if (state === 'installed') {
+          util.message('앱을 재시작하면 업데이트가 적용됩니다!');
+        }
+      });
+    });
+  });
 }
 
 
